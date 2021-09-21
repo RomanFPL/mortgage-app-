83e181c1c1985d7afc87d6cae6d45d4769ccc457
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-22 02:33:23 +0300

    Create header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../path to node modules/../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import cn from 'classnames';
 
 const App = () => {
   return (
-    <h1>Hi there...</h1>
-  )
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+        <a className="navbar-brand" href="#">Mortgage app</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">Calculate</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="#">History</a>
+            </li>
+        </ul>
+        <span className="navbar-text">
+        A place where you can calculate your mortgage
+        </span>
+        </div>
+    </div>
+    </nav>
+   );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root')
