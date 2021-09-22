ba9aa62a6e2bba3a192c31fcc8356eee5d2b718a
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-22 09:35:42 +0300

    Add form add bank validation alert

--- src/components/addBank/addBank.js
+++ src/components/addBank/addBank.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 
 import { FireBaseContext } from "../../services/firebaseContext";
+import AlertShow from "../alert";
 
 const newBank = {
     "bankName": "New",
@@ -13,39 +14,100 @@ const newBank = {
 const AddBankForm = () => {
     const firebase = useContext(FireBaseContext);
 
+    const [formVal, setFormVal] = useState({
+        formData: {
+            bankName: "",
+            ir: "",
+            ml: "",
+            mdp: "", 
+            lt: ""
+        },
+        wrongVal: false
+    });
+
+
     const handleClickSend = () => {
-        firebase.sendNewBank(newBank);
+        console.log(Object.entries(formVal).every(x => x>2));
+        setFormVal(prevState => ({
+            ...prevState,
+            wrongVal: true
+        }))
+
+        setTimeout(() => {
+            setFormVal(prevState => ({
+                ...prevState,
+                wrongVal: false
+            }))
+        },2000)
+        // firebase.sendNewBank(formInput);
+    }
+
+    const getInputValue = (e) => {
+        setFormVal(prevState => (
+            {
+                ...prevState,
+                [e.target.id]: e.target.value
+            }
+        ))     
     }
 
     return (
+        <>
         <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div className="modal-dialog">
             <div className="modal-content">
             <div className="modal-header">
-                <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <h5 className="modal-title" id="staticBackdropLabel">Add bank</h5>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
+            {formVal.wrongVal && <AlertShow/>}
             <form>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Bank name</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="bankToEnter"/>
+                    <label htmlFor="bankName" className="form-label">Bank name</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="bankName" 
+                        aria-describedby="bankToEnter"
+                        onChange={getInputValue}
+                    />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Interest rate</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="bankToEnter"/>
+                    <label htmlFor="ir" className="form-label">Interest rate</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="ir" 
+                        aria-describedby="bankToEnter"
+                        onChange={getInputValue}/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Max loan</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="bankToEnter"/>
+                    <label htmlFor="ml" className="form-label">Max loan</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="ml" 
+                        aria-describedby="bankToEnter"
+                        onChange={getInputValue}/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Max down payment</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="bankToEnter"/>
+                    <label htmlFor="mdp" className="form-label">Max down payment</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="mdp" 
+                        aria-describedby="bankToEnter"
+                        onChange={getInputValue}/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Loan term</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="bankToEnter"/>
+                    <label htmlFor="lt" className="form-label">Loan term</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="lt" 
+                        aria-describedby="bankToEnter"
+                        onChange={getInputValue}/>
                 </div>
                 <button onClick={handleClickSend} type="button" className="btn btn-primary">Add bank info</button>
             </form>
@@ -53,6 +115,7 @@ const AddBankForm = () => {
             </div>
         </div>
         </div>
+        </>
     )
 }
 
---
+++ src/components/alert/index.js
@@ -0,0 +1,11 @@
+const AlertShow = () => {
+    return (
+        <div class="alert alert-warning d-flex align-items-center" role="alert">
+        <div>
+            Your date is not valid. 
+        </div>
+        </div>
+    )
+}
+
+export default AlertShow;
