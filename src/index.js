383e260dc2c6fa1dcdc5b4349823c95bc3b20fe1
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-22 04:12:48 +0300

    Create routing withot links

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../path to node modules/../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+
 import cn from 'classnames';
 import InterestPage from './components/interestPage';
 import Navbar from './components/navBar';
 import CalcInterest from './components/calcPage';
 import HistPage from './components/histPage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-      <>
-        <Navbar/>
-    <div className="container p-4">
-        {/* <InterestPage/> */}
-        {/* <CalcInterest/> */}
-        <HistPage/>
-    </div>
-    </>
+      <Switch>
+        <Route>
+          <>
+            <Navbar/>
+            <div className="container p-4">
+              <Switch>
+              <Route path="/" component={InterestPage}/>
+              <Route path="/interests" component={InterestPage}/>
+              <Route path="/calculate" component={CalcInterest}/>
+              <Route path="/history" component={HistPage}/>
+              </Switch>
+            </div>
+          </>
+        </Route>
+      </Switch>
    );
 }
 
-ReactDOM.render(<App/>, document.getElementById('root')
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root')
 );
